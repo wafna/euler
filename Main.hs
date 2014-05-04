e30cec5e689f9ed7bc7893302959f5879cb5af7a
wafna <jfeingold@gmail.com>
2014-05-04 19:57:03 -0400

    p2

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import System.Environment
 import Data.Map(Map)
+import Data.Function(fix)
 import qualified Data.Map as Map
 
 main :: IO ()
@@ -12,9 +13,14 @@ main = do
       x : _ -> Map.findWithDefault (error $ "No problem: " ++ x) (read x) problems
 
 problems :: Map Int (IO ())
-problems = Map.fromList [(1, p1)]
+problems = Map.fromList [(1, p1), (2, p2)]
 
 -- | Find the sum of all the multiples of 3 or 5 below 1000.
 p1 :: IO ()
 p1 = putStrLn $ show $ sum [x | x <- [3 .. 999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
+
+p2 :: IO ()
+p2 = putStrLn $ show $ sum $ takeWhile ((>) 4000000) $ filter (\ x -> x `mod` 2 == 0) $ take 40 fibs
+   where
+   fibs = fix ( (1 :) . scanl (+) 2 )
